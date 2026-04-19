92752f2017419bb0864e9c982856bd68ff1b414d
1nt24is020akash-del <1nt24is020.akash@nmit.ac.in>
2026-04-19 16:46:45 +0530

    fix frontend serving

--- backend/server.js
+++ backend/server.js
@@ -36,17 +36,11 @@ app.get('/api/config/razorpay', (req, res) => res.send({ clientId: process.env.R
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get(/.*/, (req, res) =>
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get(/.*/, (req, res) =>
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+);
 
 app.use(notFound);
 app.use(errorHandler);
