56a036f52d9d7f42bc13196f83c037aad98a72e5
1nt24is020akash-del <1nt24is020.akash@nmit.ac.in>
2026-04-19 16:56:33 +0530

    remove blocking root route

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
