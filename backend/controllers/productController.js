1714c54d7748dc3cd50de2c15645aff7f252f734
1nt24is020akash-del <1nt24is020.akash@nmit.ac.in>
2026-04-26 11:06:48 +0530

    Fix ReferenceError: restore missing deleteProduct in controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -71,6 +71,18 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 const deleteProducts = asyncHandler(async (req, res) => {
   const { ids } = req.body;
 
